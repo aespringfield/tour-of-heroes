4191082af6f1944fbbfa1833009205331e2e2709
Anna Springfield <anna.e.springfield@gmail.com>
2017-08-17 11:58:51 -0500

    add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,31 @@ import { AppComponent }  from './app.component';
 import { HeroesComponent } from './heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroService } from './hero.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent],
+  imports: [ 
+    BrowserModule, 
+    FormsModule, 
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }
+    ])
+  ],
+  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent, DashboardComponent],
   providers: [ HeroService ],
   bootstrap:    [ AppComponent ]
 })
