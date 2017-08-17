84b772d18dfe9b59bee74933bc4be8255d9cb6e5
Anna Springfield <anna.e.springfield@gmail.com>
2017-08-17 09:16:09 -0500

    created heroes component and changed app component to an application shell

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
+import { HeroesComponent } from './heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent],
+  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
